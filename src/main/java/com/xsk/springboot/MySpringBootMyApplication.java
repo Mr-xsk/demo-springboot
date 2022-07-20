41c2e5c888f9ec35668341bfbab918bc54396a85
xushikai <xushikai_sinosoft@163.com>
2022-07-20 17:55:16 +0800

    1

---
+++ src/main/java/com/xsk/springboot/MySpringBootMyApplication.java
@@ -0,0 +1,19 @@
+package com.xsk.springboot;
+
+/**
+ * ｜,;,,;
+ * ｜                 ,;;'(    社
+ * ｜        __      ,;;' ' \  会
+ * ｜    /'  '\'~~'~' \ /'\.)  主
+ * ｜  ,;(      )    /  |.     义
+ * ｜ ,;' \    /-.,,(   ) \    码
+ * ｜      ) /       ) / )|    农
+ * ｜      ||        ||  \)
+ * ｜      (_\       (_
+ *
+ * @Author Mr.Xu
+ * @description:
+ * @Time 2022-07-20 14:41
+ */
+public class MySpringBootMyApplication {
+}
